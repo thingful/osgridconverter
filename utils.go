d1ed8031f2e5f5bff51bbaa4579c46a656a698f8
Popcore <smallzed@gmail.com>
2016-01-17 15:06:06 +0000

    add comments

--- utils.go
+++ utils.go
@@ -4,12 +4,12 @@ import (
 	"math"
 )
 
-// toDegrees converts radians to numeric degrees
+// toDegrees converts radians to numeric degrees.
 func toDegrees(input float64) float64 {
 	return input * 180 / math.Pi
 }
 
-// toRadians converts numeric degrees to radians
+// toRadians converts numeric degrees to radians.
 func toRadians(input float64) float64 {
 	return input * math.Pi / 180
 }
